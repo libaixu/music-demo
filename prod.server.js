7fd2753db03f69a96ee749fda1b57ce984f4c5dd
libaixu <LJ33LJ@163.com>
2018-09-03 15:00:54 +0800

    add

--- prod.server.js
+++ prod.server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 const axios = require('axios')
 const compression = require('compression')
 const app = express()
